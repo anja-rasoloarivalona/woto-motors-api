7a7f2a3b477dfa86cab8438bb0d0ca7c3fd784e2
anja-rasoloarivalona <rasoloanja@gmail.com>
2019-09-24 13:31:52 -0400

    Added logic to save product details into mongoDB

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,60 @@
+const Product = require('../models/product');
+
+
 exports.addProduct = (req, res, next) => {
     console.log('adding...');
 
-    console.log('body', req.body)
+
+    let imageUrlsString = req.body.imageUrls;
+    let featuresString = req.body.features;
+
+    
+    let imageUrlsArray = imageUrlsString.split(',');
+    let featuresArray = featuresString.split(',');
+
+
+    const product = new Product({
+        general: [{
+            title: req.body.title,
+            made: req.body.made,
+            model: req.body.model,
+            year: req.body.year,
+            price: req.body.price,
+            nbKilometers: req.body.nbKilometers,
+            gazol: req.body.gazol,
+            yearOfRelease: req.body.yearOfRelease,
+            nbOwners: req.body.nbOwners,
+            serialNumber: req.body.serialNumber,
+            generalState: req.body.generalState
+        }],
+
+        tech: [{
+            nbGearRatios: req.body.nbGearRatios,
+            nbCylindres: req.body.nbCylinders,
+            motorSize: req.body.motorSize,
+            maxSpeed: req.body.maxSpeed,
+        }],
+
+        design: [{
+            intColor: req.body.intColor,
+            extColor: req.body.extColor
+        }],
+
+        features: featuresArray, 
+        imageUrls: imageUrlsArray
+    })
+
+
+    product
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Product created successfully!',
+                product: product
+            })
+        }) 
+        .catch(err => {
+            console.log(err)
+        })
+
 }
---
+++ models/product.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const productSchema = new Schema({
+
+    general: [{
+        title: {
+            type: String,
+            required: true
+        },
+        
+        made: {
+            type: String,
+            required: true
+        },
+
+        model: {
+            type: String,
+            required: true
+        },
+
+        year: {
+            type: String,
+            required: true
+        },
+
+        price: {
+            type: Number,
+            required: true
+        },
+
+        nbKilometers: Number,
+        gazol: String,
+        yearOfRelease: String,
+        nbOwners: Number,
+        serialNumber: String,
+        generalState: String
+    
+    }],
+
+
+    tech: [{
+        nbGearRatios: Number,
+        nbCylinders: Number,
+        motorSize: String,
+        maxSpeed: String
+    }],
+
+    design: [{
+        intColor: String,
+        extColor: String
+    }],
+
+    features: [{
+        type: String
+    }],
+
+    imageUrls: [{
+        type: String
+    }]
+
+   
+}, {timestamps: true})
+
+
+
+module.exports = mongoose.model('Product', productSchema)
