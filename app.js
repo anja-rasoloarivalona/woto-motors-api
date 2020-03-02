a2fcc66b05b4fb7afd280987725c647a9d3fd31d
anja <rasoloanja@gmail.com>
2020-03-02 11:16:56 -0500

    Fixed bug

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*')
     next();
 });
   
